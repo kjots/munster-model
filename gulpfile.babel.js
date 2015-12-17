0062e1b512654bb572e2f868baa542bc4b68aa02
Karl Juri Ots <kjots@kjots.org>
2015-12-17 17:58:43 +1100

    Added watch task to Gulp script.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,22 +2,32 @@
 
 import 'babel-polyfill';
 
+import chokidar from 'chokidar';
 import del from 'del';
 import gulp from 'gulp';
 import gulpBabel from 'gulp-babel';
 import vinylPaths from 'vinyl-paths';
 
-gulp.task('compile', () => {
+gulp.task('watch', [ 'compile' ], watch);
+function watch() {
+    chokidar.watch('src/**/*.js', { ignoreInitial: true }).on('all', function (event, file) {
+        compile();
+    });
+}
+
+gulp.task('compile', [], compile);
+function compile() {
     return gulp.src('src/**/*.js')
         .pipe(gulpBabel())
         .pipe(gulp.dest('lib'));
-});
+}
 
-gulp.task('clean', [], () => {
+gulp.task('clean', [], clean);
+function clean() {
     var stream = gulp.src([
         'lib'
     ], { read: false });
 
     return stream
         .pipe(vinylPaths(del));
-});
+}
